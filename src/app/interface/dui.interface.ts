b426b1a6e89ea42317e10bd48e10cd43c94adf95
stephany.pineda <stephany.pineda@datumredsoft.com>
2022-07-05 15:08:41 -0600

    urls

---
+++ src/app/interface/dui.interface.ts
@@ -0,0 +1,58 @@
+export interface DatosPartidaResponse {
+    dui:         string;
+    noSoli:      string;
+    nom1:        string;
+    nom2:        string;
+    ape1:        string;
+    ape2:        string;
+    sexo:        string;
+    nomMadre:    string;
+    nomPadre:    string;
+    status:      string;
+    marginacion: string;
+    rollo:       string;
+    rolloSec:    null;
+    anio:        string;
+    libro:       string;
+    folio:       string;
+    partida:     string;
+    deptoLibro:  string;
+    muniLibro:   string;
+    fecNac:      string;
+    deptoNac:    string;
+    muniNac:     string;
+    cuadro:      string;
+}
+
+
+export interface DatosDuiResponse {
+    dui:                string;
+    numeroSolicitud:    string;
+    nom1:               string;
+    nom2:               string;
+    ape1:               string;
+    ape2:               string;
+    sexo:               string;
+    estadoFami:         string;
+    conyugue:           null;
+    nombMadr:           string;
+    nombPadr:           string;
+    fechNaci:           string;
+    nombPaisNaci:       string;
+    nombDeptNaci:       string;
+    nombMuniNaci:       string;
+    estadoCiudadano:    string;
+    profesion:          string;
+    peso:               string;
+    estatura:           string;
+    conocidoPor:        null;
+    residencia:         string;
+    calle:              string;
+    casa:               string;
+    zip:                string;
+    nombPaisResidencia: string;
+    nombDeptResidencia: string;
+    nombMuniResidencia: string;
+    canton:             null;
+    caserio:            null;
+}
--- src/app/services/dui.service.ts
+++ src/app/services/dui.service.ts
@@ -1,34 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DatosDuiResponse, DatosPartidaResponse } from '../interface/dui.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DuiService {
 
-  private _url:string = '';
-
   private _datosDui:any = {};
   private _datosTramites:any = {};
   private _datosPartida:any = {};
 
   constructor( private http: HttpClient) { 
-     //Llamada a los 
-     this.http.get('https://api.giphy.com/v1/gifs/search?apiKey=1KyNl3fjOwA3pxGhW8Xh7zqbYYLmx7CC&q=cheese&limit=10')
-        .subscribe( (resp: any) => {
-            console.log( resp );
-        } )
+
+    const dui = '044021771';
+    const urlDatosDUI:string = `https://service-datos-dui-3-api-datos-personas.apps-crc.testing/api/datosdui?dui=${dui}`;
+    //const urlDatosTramite:string = `?dui=${dui}`;
+    const urlDatosPartida:string = `https://service-datos-partida-1-api-datos-personas.apps-crc.testing/api/datospartida?dui=${dui}`;
+
+     this.http.get<DatosDuiResponse>(urlDatosDUI )
+        .subscribe( (resp ) => {
+            console.log( 'Datos Dui' + resp );
+            this._datosDui = resp; 
+      } )
+
+      this.http.get<DatosPartidaResponse>(urlDatosPartida)
+        .subscribe( (resp) => {
+            console.log( 'Datos partida: '+ resp );
+            this._datosPartida = resp;
+      } )
   }
 
   get datodDui() {
-    return this._datosDui;
+    return {...this._datosDui};
   }
 
   get datosTramite() {
-    return this._datosTramites;
+    return {...this._datosTramites};
   }
 
   get datosPartida() {
-    return this._datosPartida;
+    return {...this._datosPartida};
   }
 }
