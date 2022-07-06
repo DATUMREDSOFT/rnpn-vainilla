bcd0c90044bdb5ca51541e2aac9b601ab9ed3bfb
stephany.pineda <stephany.pineda@datumredsoft.com>
2022-07-05 18:31:25 -0600

    Query params

--- src/app/services/dui.service.ts
+++ src/app/services/dui.service.ts
@@ -13,10 +13,9 @@ export class DuiService {
 
   constructor( private http: HttpClient) { 
 
-    const dui = '044021771';
-    const urlDatosDUI:string = `https://service-datos-dui-3-api-datos-personas.apps-crc.testing/api/datosdui?dui=${dui}`;
-    const urlDatosTramite:string = `https://service-datos-tramite-1-api-datos-personas.apps-crc.testing/api/datostramite?dui=${dui}`;
-    const urlDatosPartida:string = `https://service-datos-partida-1-api-datos-personas.apps-crc.testing/api/datospartida?dui=${dui}`;
+    const urlDatosDUI:string = `https://service-datos-dui-3-api-datos-personas.apps-crc.testing/api/datosdui`;
+    const urlDatosTramite:string = `https://service-datos-tramite-1-api-datos-personas.apps-crc.testing/api/datostramite`;
+    const urlDatosPartida:string = `https://service-datos-partida-1-api-datos-personas.apps-crc.testing/api/datospartida`;
 
     let parameters = {"dui":"044021771"}
     let queryParams = new HttpParams({ fromObject: parameters }); 
@@ -27,13 +26,13 @@ export class DuiService {
             this._datosDui = resp; 
       } )
 
-      this.http.get<DatosPartidaResponse>(urlDatosDUI, {params:queryParams} )
+      this.http.get<DatosPartidaResponse>(urlDatosPartida, {params:queryParams} )
         .subscribe( (resp) => {
             console.log( 'Datos partida: '+ resp );
             this._datosPartida = resp;
       } )
 
-      this.http.get<DatosTramiteResponse>(urlDatosDUI, {params:queryParams} )
+      this.http.get<DatosTramiteResponse>(urlDatosTramite, {params:queryParams} )
       .subscribe( (resp) => {
           console.log( 'Datos tramite: '+ resp );
           this._datosTramites = resp;
